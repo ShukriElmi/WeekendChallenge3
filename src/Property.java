7d99d0a5d9efcbee8e5f8e3db764c8f29a7a0e75
Shukri Elmi <shukrielmi@Shukris-MacBook-Air.local>
2022-11-06 18:00:26 -0600

    Property class intiated

--- src/Property.java
+++ src/Property.java
@@ -1,2 +1,16 @@
 public class Property {
+    String name;
+    String location;
+    double price;
+
+    public Property (String name, String location, double price) {
+        this.name = name;
+        this.location = location;
+        this.price = price;
+    }
+
+    public void print(){
+        System.out.println("The name of the this property " + name + " and the location of this property is " + location + " and the price is " + price);
+    }
 }
+
