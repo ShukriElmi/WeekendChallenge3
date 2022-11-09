615fde7b0981c986658afef976c1987a3e8ed855
Shukri Elmi <shukrielmi@Shukris-MacBook-Air.local>
2022-11-09 13:37:03 -0600

    created getters and setters

--- src/Property.java
+++ src/Property.java
@@ -1,8 +1,41 @@
 public class Property {
     private String name;
     private String location;
-   private  double price;
+    private  double price;
+
     private int idNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
 
     public Property (String name, String location, double price) {
         this.name = name;
