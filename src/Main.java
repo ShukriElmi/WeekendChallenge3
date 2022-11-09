ac4f74e35e4115539005cde6800efd52d22f85ce
Shukri Elmi <shukrielmi@Shukris-MacBook-Air.local>
2022-11-09 13:28:12 -0600

    added 'quit' function

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,22 @@ public class Main {
     static Airbnb shukriAirbnb = new Airbnb();
     static String option = "";
     public static void main(String[] args) {
+        shukriAirbnb.greeting();
+        option = scan.nextLine();
+
+        if (option.equalsIgnoreCase("C")){
+            shukriAirbnb.createProperty();
+            shukriAirbnb.saveData();
+        } else if (option.equalsIgnoreCase("R")) {
+            shukriAirbnb.readProperty();
+            shukriAirbnb.loadData();
+        } else if (option.equalsIgnoreCase("U")) {
+            shukriAirbnb.updateProperty();
+        } else if (option.equalsIgnoreCase("D")){
+            shukriAirbnb.deleteProperty();
+        } else if (option.equalsIgnoreCase("Q")) {
+            System.out.println("Would you like to see the properties?");
+        }
 
     }
 }
